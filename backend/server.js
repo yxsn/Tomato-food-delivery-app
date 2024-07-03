757c669590034e25c505eec983824b04d2cd1bc7
Yasin N K <yasinnk15@gmail.com>
2024-07-03 22:59:49 +0530

    minor issue fix

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: [""],
+  methods:["POST","GET"],
+  credentials:true
+}))
 
 // db connection
 connectDB()
