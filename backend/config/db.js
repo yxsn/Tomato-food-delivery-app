2ae4fe8fa6c107deee029849448f280c543e0fbd
Yasin N K <yasinnk15@gmail.com>
2024-07-04 00:23:21 +0530

    updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://yasinnk15:Naduvilekovilakath1@cluster0.tjpgsze.mongodb.net/food-del"
-    )
+    .connect(process.env.MONGODB_PASSWORD)
     .then(() => console.log("DB connected"));
 };
