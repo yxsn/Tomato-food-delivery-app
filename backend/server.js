30b94746bd2d2c12401b8c1c5b719fcd0dd86588
Yasin N K <yasinnk15@gmail.com>
2024-07-04 01:10:46 +0530

    error fixes

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,7 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: [""],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // db connection
 connectDB();
