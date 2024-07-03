a021a608d607e85b82c9cade5fea7cc9d11ad1d5
Yasin N K <yasinnk15@gmail.com>
2024-07-04 01:20:15 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://tomato-food-delivery-app-backend.onrender.com'
 export const currency = '₹'
 
 export const assets ={
