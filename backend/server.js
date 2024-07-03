e6058f70f47333d9b342af2eb7d3c87ce57a63fb
Yasin N K <yasinnk15@gmail.com>
2024-07-03 23:36:12 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,7 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
-app.use(cors({
-  origin: [""],
-  methods:["POST","GET"],
-  credentials:true
-}))
+app.use(cors())
 
 // db connection
 connectDB()
